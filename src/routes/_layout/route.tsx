d43d6049478f0cfd6f07f8eafe716e531646a52a
Paulo H. A. Leite <paulohaleite@gmail.com>
2026-04-23 15:59:01 -0300

    chore: aplica formatacao biome 2.4.5 no codebase (#1)
    * chore(biome): atualiza schema para 2.4.5
    
    * style: aplica formatacao biome (tabs, aspas duplas, semicolons)

--- src/routes/_layout/route.tsx
+++ src/routes/_layout/route.tsx
@@ -1,15 +1,15 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
-import { Header } from '#/components/Layout'
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { Header } from "#/components/Layout";
 
-export const Route = createFileRoute('/_layout')({
-  component: Layout,
-})
+export const Route = createFileRoute("/_layout")({
+	component: Layout,
+});
 
 function Layout() {
-  return (
-    <>
-      <Header />
-      <Outlet />
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<Outlet />
+		</>
+	);
 }
